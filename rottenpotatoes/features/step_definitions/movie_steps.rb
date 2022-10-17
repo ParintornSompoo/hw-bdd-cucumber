62de9868dd508e1308c7ecd8fb7122c446ebae93
ParintornSompoo <s6301012610060@email.kmutnb.ac.th>
2022-10-17 20:37:55 +0700

    part 3 : finish Scenario restrict to movies with 'PG' or 'R' ratings

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -29,7 +29,15 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  if uncheck.nil?
+    rating_list.split(',').each do |rating|
+      step "I check \"ratings[#{rating}]\""
+    end
+  else
+    rating_list.split(',').each do |rating|
+      step "I uncheck \"ratings[#{rating}]\""
+    end
+  end
 end
 
 Then /I should see all the movies/ do
