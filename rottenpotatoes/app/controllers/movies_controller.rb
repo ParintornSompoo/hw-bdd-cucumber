a491893658b755618b538588f50b872a0909bf74
hill <hill@core>
2022-11-16 12:28:52 +0700

    Clear unnecessary comment

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -21,13 +21,7 @@ class MoviesController < ApplicationController
     when 'release_date'
       ordering,@date_header = {:release_date => :asc}, 'bg-warning hilite'
     end
-    # @all_ratings = Movie.all_ratings
     @selected_ratings = params[:ratings] || session[:ratings] || Hash[Movie.all_ratings.map {|rating| [rating, rating]}]
-
-    # if @selected_ratings == {}
-    #   @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
-    # end
-
     if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
       session[:sort] = sort
       session[:ratings] = @selected_ratings
